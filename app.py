9bbb5430d7ec8def172817e3d426b7ea151488b6
naeem_hussien <naeem.hussien@bebit.com>
2020-09-17 22:03:51 +0900

    add streamlit file

---
+++ app.py
@@ -0,0 +1,123 @@
+import streamlit as st 
+import numpy as np 
+
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import accuracy_score
+
+st.title('Interview with Rakuten Logistics IncubAI')
+
+st.write("""
+### Naeem, beBit Machine Learning Engineer
+""")
+st.subheader(""" What I have done/currently doing""")
+
+st.write(""" • Tuned algorithms based on statistical analysis and predictive data modelling to strengthen longevity and personalize interactions for clients \n
+• Developed two features using statistical modelling and machine learning techniques to inform client’s strategic decisions \n
+• Identified abnormalities encountered by client’s user while performing an action \n
+• Predict client’s user which high possibilities of making an action\n
+• Built four KPI dashboards to measure features engagement with the user \n
+• Scrum Master for the team of 10 engineers """)
+
+st.subheader("""Python, SQL, AWS, Continuous Integrations (Github Actions), Docker & Git, Kubernetes""")
+
+st.write("""#### PythonAnywhere: https://naeemhussien.pythonanywhere.com
+#### Blog: https://naeem-bebit.github.io/
+""")
+
+st.subheader(""" DEMO""")
+dataset_name = st.sidebar.selectbox(
+    'Select Dataset',
+    ('Iris', 'Breast Cancer', 'Wine')
+)
+
+st.write(f"## {dataset_name} Dataset")
+
+classifier_name = st.sidebar.selectbox(
+    'Select classifier',
+    ('KNN', 'SVM', 'Random Forest')
+)
+
+def get_dataset(name):
+    data = None
+    if name == 'Iris':
+        data = datasets.load_iris()
+    elif name == 'Wine':
+        data = datasets.load_wine()
+    else:
+        data = datasets.load_breast_cancer()
+    X = data.data
+    y = data.target
+    return X, y
+
+X, y = get_dataset(dataset_name)
+st.write('Shape of dataset:', X.shape)
+st.write('number of classes:', len(np.unique(y)))
+
+def add_parameter_ui(clf_name):
+    params = dict()
+    if clf_name == 'SVM':
+        C = st.sidebar.slider('C', 0.01, 10.0)
+        params['C'] = C
+    elif clf_name == 'KNN':
+        K = st.sidebar.slider('K', 1, 15)
+        params['K'] = K
+    else:
+        max_depth = st.sidebar.slider('max_depth', 2, 15)
+        params['max_depth'] = max_depth
+        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
+        params['n_estimators'] = n_estimators
+    return params
+
+params = add_parameter_ui(classifier_name)
+
+def get_classifier(clf_name, params):
+    clf = None
+    if clf_name == 'SVM':
+        clf = SVC(C=params['C'])
+    elif clf_name == 'KNN':
+        clf = KNeighborsClassifier(n_neighbors=params['K'])
+    else:
+        clf = clf = RandomForestClassifier(n_estimators=params['n_estimators'], 
+            max_depth=params['max_depth'], random_state=1234)
+    return clf
+
+clf = get_classifier(classifier_name, params)
+#### CLASSIFICATION ####
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+acc = accuracy_score(y_test, y_pred)
+
+st.write(f'Classifier = {classifier_name}')
+st.write(f'Accuracy =', acc)
+
+#### PLOT DATASET ####
+# Project the data onto the 2 primary principal components
+pca = PCA(2)
+X_projected = pca.fit_transform(X)
+
+x1 = X_projected[:, 0]
+x2 = X_projected[:, 1]
+
+fig = plt.figure()
+plt.scatter(x1, x2,
+        c=y, alpha=0.8,
+        cmap='viridis')
+
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.colorbar()
+
+#plt.show()
+st.pyplot()
